60949fcc285e460f4d80e598f5fbb493abdd7bc8
mateuszpuzniak <mpuzniak24@gmail.com>
2023-05-17 10:26:34 +0200

    update wersja podstawowa

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import HomeHeader from './HomeHeader'
 import HomeNavigation from './HomeNavigation'
 import { ReactComponent as Decoration } from '../assets/Decoration.svg'
-import { Link } from 'react-router-dom';
+
+function validateEmail(email) {
+	const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	return emailRegex.test(email)
+}
 
 function Login() {
+	const [email, setEmail] = useState('')
+	const [isValidEmail, setValidEmail] = useState(null)
+	const [isValidPass, setValidPass] = useState(null)
+
+
+	const handleChangeEmail = event => {
+		setEmail(event.target.value)
+		setValidEmail(null)
+	}
+	const handleChangePass = event => {
+		const text = event.target.value
+		setValidPass(text.length >= 6)
+	}
+
+	const handleSubmit = event => {
+		event.preventDefault()
+		const isEmailValid = validateEmail(email)
+
+		setValidEmail(isEmailValid)
+
+		if (isEmailValid && isValidPass) {
+			console.log('Logged In')
+		}
+	}
+
 	return (
 		<div>
 			<div>
@@ -17,21 +47,28 @@ function Login() {
 						<h2 className='fourStepsH2' style={{fontSize:"30px", fontWeight:"normal",textAlign:"center"}}>Zaloguj się</h2>
                         <Decoration style={{margin:"40px 0px"}}/>
 					</div>
-					<div style={{background:"#F0F1F1 0% 0% no-repeat padding-box", width:"350px", height:"200px", display:"flex", flexDirection:"column",justifyContent:"center", alignItems:"center"}}>
-                        <div style={{display:"flex", flexDirection:"column"}}>
-                            <label className='loginLabel' for="username">Email:</label>
-                            <input className='loginInput' type="text" id="username" name="username" style={{width:"200px"}}/>
+					<form onSubmit={handleSubmit} style={{display:"flex", justifyContent:"center",alignItems:"center",flexDirection:"column"}} >
+						<div style={{background:"#F0F1F1", width:"350px", height:"240px", display:"flex", flexDirection:"column",justifyContent:"center", alignItems:"center"}}>
+                        	<div style={{display:"flex", flexDirection:"column"}}>
+                            	<label className='loginLabel' htmlFor="username">Email:</label>
+                            	<input className='loginInput' type="email" id="username" name="username" onChange={handleChangeEmail} style={{width:"200px"}}/>
+							{isValidEmail === false && (
+										<div style={{ color: 'red', marginBottom: '20px' }}>Podany adres email jest nieprawidłowy.</div>
+									)}
+                        	</div>
+                        	<div style={{display:"flex", flexDirection:"column"}}>
+                            	<label className='loginLabel' htmlFor="pass">Hasło</label>
+                            	<input onChange={handleChangePass} className='loginInput' type="password" id="pass" name="password" style={{width:"200px"}}/>
+								{isValidPass === false && ( <div style={{ color: 'red',  lineBreak:"auto" }}>Hasło musi zawierać <br /> co najmniej 6 znaków.</div>)}
+								{isValidPass && (<div style={{ color: 'green', marginBottom: '20px',}}>Hasło spełniające wymogi.</div>)}
+                        	</div>
+                    	</div>
+						<div style={{display:"flex", justifyContent:"space-around",alignItems:"center",width:"700px"}}>
+                            <button className='loginBtn' type='submit' style={{padding:"10px 20px", marginTop:"30px"}}>Zaloguj się</button>
+                            <Link to='/rejestracja' className='loginBtn'>
+                            Załóż konto</Link>
                         </div>
-                        <div style={{display:"flex", flexDirection:"column"}}>
-                            <label className='loginLabel' for="pass">Hasło</label>
-                            <input className='loginInput' type="password" id="pass" name="password" style={{width:"200px"}}/>
-                        </div>
-                    </div>
-					<div style={{display:"flex", justifyContent:"space-around",alignItems:"center",width:"700px"}}>
-                        <button className='loginBtn' style={{padding:"10px 20px", marginTop:"30px"}}>Zaloguj się</button>
-						<Link to='/rejestracja' className='loginBtn'>
-						Załóż konto</Link>
-                    </div>
+					</form>
 				</div>
 			</div>
 		</div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import HomeHeader from './HomeHeader'
 import HomeNavigation from './HomeNavigation'
 import { ReactComponent as Decoration } from '../assets/Decoration.svg'
 
+function validateEmail(email) {
+	const emailRegex =
+		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	return emailRegex.test(email)
+}
+
 function Register() {
+	const [email, setEmail] = useState('')
+	const [isValidEmail, setValidEmail] = useState(null)
+	const [isValidPassOne, setValidPassOne] = useState(null)
+	const [isValidPassTwo, setValidPassTwo] = useState(null)
+    const [passwordOne, setPasswordOne] = useState('');
+
+	const handleChangeEmail = event => {
+		setEmail(event.target.value)
+		setValidEmail(null)
+	}
+
+	const handleChangePassOne = event => {
+		const text = event.target.value
+        setPasswordOne(text);
+		setValidPassOne(text.length >= 6)
+	}
+
+	const handleChangePassTwo = event => {
+		const text = event.target.value
+        const isPasswordMatch = text === passwordOne;
+        setValidPassTwo(text.length >= 6 && isPasswordMatch);
+	}
+
+	const handleSubmit = event => {
+		event.preventDefault()
+		const isEmailValid = validateEmail(email)
+
+		setValidEmail(isEmailValid)
+
+		if (isEmailValid && isValidPassOne && isValidPassTwo) {
+			console.log('Logged In')
+		}
+	}
+
 	return (
 		<div>
 			<div>
@@ -12,29 +52,114 @@ function Register() {
 					<HomeHeader />
 					<HomeNavigation />
 				</div>
-				<div style={{display:"flex", justifyContent:"center",alignItems:"center", flexDirection:"column",marginTop:"70px"}}>
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						flexDirection: 'column',
+						marginTop: '70px',
+					}}>
 					<div>
-						<h2 className='fourStepsH2' style={{fontSize:"30px", fontWeight:"normal",textAlign:"center"}}>Zarejestruj się</h2>
-                        <Decoration style={{margin:"40px 0px"}}/>
+						<h2
+							className='fourStepsH2'
+							style={{
+								fontSize: '30px',
+								fontWeight: 'normal',
+								textAlign: 'center',
+							}}>
+							Zarejestruj się
+						</h2>
+						<Decoration style={{ margin: '40px 0px' }} />
 					</div>
-					<div style={{background:"#F0F1F1 0% 0% no-repeat padding-box", width:"350px", height:"300px", display:"flex", flexDirection:"column",justifyContent:"center", alignItems:"center"}}>
-                        <div style={{display:"flex", flexDirection:"column"}}>
-                            <label className='loginLabel' for="username">Email:</label>
-                            <input className='loginInput' type="text" id="username" name="username" style={{width:"200px"}}/>
-                        </div>
-                        <div style={{display:"flex", flexDirection:"column"}}>
-                            <label className='loginLabel' for="pass">Hasło</label>
-                            <input className='loginInput' type="password" id="pass" name="password" style={{width:"200px"}}/>
-                        </div>
-                        <div style={{display:"flex", flexDirection:"column"}}>
-                            <label className='loginLabel' for="pass">Powtórz hasło</label>
-                            <input className='loginInput' type="password" id="pass" name="password" style={{width:"200px"}}/>
-                        </div>
-                    </div>
-					<div style={{display:"flex", justifyContent:"space-around",alignItems:"center",width:"700px"}}>
-                        <button className='loginBtn' style={{padding:"10px 20px", marginTop:"30px"}}>Załóż konto</button>
-                        <Link to='/logowanie' className='loginBtn'>Zaloguj się</Link>
-                    </div>
+					<form
+						onSubmit={handleSubmit}
+						style={{
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							flexDirection: 'column',
+						}}>
+						<div
+							style={{
+								background: '#F0F1F1',
+								width: '350px',
+								height: '370px',
+								display: 'flex',
+								flexDirection: 'column',
+								justifyContent: 'center',
+								alignItems: 'center',
+							}}>
+							<div style={{ display: 'flex', flexDirection: 'column' }}>
+								<label className='loginLabel' htmlFor='username'>
+									Email:
+								</label>
+								<input
+									onChange={handleChangeEmail}
+									className='loginInput'
+									type='email'
+									id='emailInput'
+									placeholder='abc@xyz.pl'
+									style={{ marginBottom: '10px', fontSize: '18px', width: '220px' }}
+								/>
+								{isValidEmail === false && (
+									<div style={{ color: 'red', marginBottom: '20px' }}>Podany adres email jest nieprawidłowy.</div>
+								)}
+								{isValidEmail && <div style={{ color: 'green', marginBottom: '20px' }}>Adres email jest poprawny.</div>}
+							</div>
+							<div style={{ display: 'flex', flexDirection: 'column' }}>
+								<label className='loginLabel' htmlFor='pass'>
+									Hasło
+								</label>
+								<input
+                                    value={passwordOne}
+									onChange={handleChangePassOne}
+									className='loginInput'
+									type='password'
+									name='password'
+									style={{ width: '220px' }}
+								/>
+								{isValidPassOne === false && (
+									<div style={{ color: 'red', lineBreak: 'auto' }}>Hasło musi zawierać <br /> co najmniej 6 znaków.</div>
+								)}
+								{isValidPassOne && (
+									<div style={{ color: 'green', marginBottom: '20px' }}>Hasło spełniające wymogi.</div>
+								)}
+							</div>
+							<div style={{ display: 'flex', flexDirection: 'column' }}>
+								<label className='loginLabel' htmlFor='pass'>
+									Powtórz hasło
+								</label>
+								<input
+									onChange={handleChangePassTwo}
+									className='loginInput'
+									type='password'
+									name='password'
+									style={{ width: '220px' }}
+								/>
+								{isValidPassTwo === false && (
+									<div style={{ color: 'red', lineBreak: 'auto' }}>Hasła nie są identyczne lub puste.</div>
+								)}
+								{isValidPassTwo && (
+									<div style={{ color: 'green', marginBottom: '20px' }}>Hasło spełniające wymogi.</div>
+								)}
+							</div>
+						</div>
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'space-around',
+								alignItems: 'center',
+								width: '700px',
+							}}>
+							<button className='loginBtn' type='submit' style={{ padding: '10px 20px', marginTop: '30px' }}>
+								Załóż konto
+							</button>
+							<Link to='/logowanie' className='loginBtn'>
+								Zarejestrowano
+							</Link>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
